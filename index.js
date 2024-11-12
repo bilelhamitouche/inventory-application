f223494bff7709bb822081e77f5f679e849edcd5
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-13 00:23:52 +0100

    add views and view path

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const path = require("path");
 const indexRouter = require("./routes/indexRouter");
+const brandRouter = require("./routes/brandRouter");
+const modelRouter = require("./routes/modelRouter");
 require("dotenv").config();
 
 const app = express();
@@ -7,12 +10,11 @@ const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use("/", indexRouter);
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/brands", brandRouter);
+app.use("/models", modelRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
