f1b9abd50272fd08dfda871f4fc8cc7633de2484
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-13 18:18:12 +0100

    add ssl setting

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,6 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  port: process.env.PGPORT,
-  password: process.env.PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
