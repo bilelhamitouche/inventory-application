22a9fae256fab06b020d2da5f3e4627f140d9e0a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-13 17:58:44 +0100

    add router endpoints for delete, post and get.

--- routes/brandRouter.js
+++ routes/brandRouter.js
@@ -2,11 +2,17 @@ const express = require("express");
 const {
   listBrands,
   createBrandGet,
+  createBrandPost,
+  deleteBrand,
+  listModelsPerBrand,
 } = require("../controllers/brandController");
 
 const brandRouter = express.Router();
 
 brandRouter.get("/", listBrands);
 brandRouter.get("/create", createBrandGet);
+brandRouter.post("/create", createBrandPost);
+brandRouter.get("/:id", listModelsPerBrand);
+brandRouter.post("/:id/delete", deleteBrand);
 
 module.exports = brandRouter;
--- routes/modelRouter.js
+++ routes/modelRouter.js
@@ -2,11 +2,15 @@ const express = require("express");
 const {
   listModels,
   createModelGet,
+  createModelPost,
+  deleteModel,
 } = require("../controllers/modelController");
 
 const modelRouter = express.Router();
 
 modelRouter.get("/", listModels);
 modelRouter.get("/create", createModelGet);
+modelRouter.post("/create", createModelPost);
+modelRouter.post("/:id/delete", deleteModel);
 
 module.exports = modelRouter;
